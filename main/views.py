94ecd3e34d65b92210ff2919e705a751aa0ff0f0
Илья <ilyaban2004@gmail.com>
2024-05-17 19:55:24 +0200

    After adding fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context: dict = {
         'title': 'OnlineShop - Главная страница',
-        'content': "OnlineShop: Онлайн-магазин для всех ваших потребностей"
+        'content': "OnlineShop: Онлайн-магазин для всех ваших потребностей",
+        'categories': categories
     }
 
     return render(request, 'main/index.html', context)
