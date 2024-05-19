15cd0ecf7a6118761f9b5182cc154eca2ce08e10
Илья <ilyaban2004@gmail.com>
2024-05-19 13:15:00 +0200

    Slug converter for categories

--- goods/models.py
+++ goods/models.py
@@ -22,7 +22,7 @@ class Products(models.Model):
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
     discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка в %')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
-    category = models.ForeignKey(to=Categories,on_delete=models.CASCADE, verbose_name='Категория')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
     class Meta:
         db_table = 'products'
